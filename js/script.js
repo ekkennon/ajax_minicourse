dcb63dcd6b6a48f8e184b3cdb8cf6b9d1c4ab600
Erin K. Kennon <ekkennon@gmail.com>
2017-05-29 18:36:33 -0500

    finished prep work 7

--- js/script.js
+++ js/script.js
@@ -11,9 +11,41 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+
+    var street = $("#street").val();
+    var city = $("#city").val();
+    var streetViewUrl = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + street + ", " + city;
+    var nytimesUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + city + "&sort=newest&api-key=111d65d59b9b452eb26f748a5bb8476a";
+    var wikipediaUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + city + "&format=json&callback=wikiCallback";
     // load streetview
+    
+    $body.append("<img class='bgimg' src='" + streetViewUrl + "' />");
 
     // YOUR CODE GOES HERE!
+    $.getJSON(nytimesUrl, function(data) {
+        $nytHeaderElem.text("New York Times Articles About " + city);
+        var nyarticles = data.response.docs;
+        for (var i=0;i<nyarticles.length;i++) {
+            var nyarticle = nyarticles[i];
+            $nytElem.append("<li class='article'><a href='" + nyarticle.web_url + "'>" + nyarticle.headline.main + "</a><p>" + nyarticle.snippet + "</p></li>");
+
+        }
+    }).error(function(e) {
+        $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
+    });
+
+    var wikiRequestTimeout = setTimeout(function() {
+        $wikiElem.text("Failed To Get Wikipedia Resources");
+    }, 8000);
+    $.ajax({url: wikipediaUrl, dataType: "jsonp", success: function(response) {
+        var warticles = response[1];
+        for (var i=0;i<warticles.length;i++) {
+            var warticle = warticles[i];
+            var wurl = "http://en.wikipedia.org/wiki/" + warticle;
+            $wikiElem.append("<li><a href='" + wurl + "'>" + warticle + "</a></li>");
+        }
+        clearTimeout(wikiRequestTimeout);
+    }});
 
     return false;
 };
